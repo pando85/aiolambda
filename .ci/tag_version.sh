a5be18fc8906d9fa109904f3ae19bfc9c3a6158f
pando85 <pando855@gmail.com>
2018-12-13 01:29:01 +0100

    Fix: replace python for bash to get version (#5)

--- .ci/tag_version.sh
+++ .ci/tag_version.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -z "${WORKON_HOME}" ]; then
-    PYTHON='python3'
-else
-    PYTHON=${WORKON_HOME}/aiolambda/bin/python3
-fi
-
-
 check_version_changes(){
     git diff  HEAD^ HEAD -- aiolambda/__init__.py | grep --quiet +__version__;
 };
@@ -16,7 +9,7 @@ if ! check_version_changes; then
     echo "Not version changed"
     exit
 fi
-VERSION=$(${PYTHON} -c 'import aiolambda; print(aiolambda.__version__)')
+VERSION=$(grep version aiolambda/__init__.py | cut -d= -f2 | tr -d '[:space:]'| tr -d "'")
 
 git tag -a $VERSION -m "version $VERSION"
 git push --follow-tags "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:$TRAVIS_BRANCH
