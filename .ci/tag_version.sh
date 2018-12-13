02f629ac8dd1dc630a5be3a88dbf47c7238ec8cd
pando85 <pando855@gmail.com>
2018-12-13 01:10:59 +0100

    Fix travis tag version (#4)

--- .ci/tag_version.sh
+++ .ci/tag_version.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ -z "${WORKON_HOME}" ]; then
+    PYTHON='python3'
+else
+    PYTHON=${WORKON_HOME}/aiolambda/bin/python3
+fi
+
+
 check_version_changes(){
     git diff  HEAD^ HEAD -- aiolambda/__init__.py | grep --quiet +__version__;
 };
@@ -9,7 +16,7 @@ if ! check_version_changes; then
     echo "Not version changed"
     exit
 fi
-VERSION=$(python -c 'import aiolambda; print(aiolambda.__version__)')
+VERSION=$(${PYTHON} -c 'import aiolambda; print(aiolambda.__version__)')
 
 git tag -a $VERSION -m "version $VERSION"
 git push --follow-tags "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG" HEAD:$TRAVIS_BRANCH
