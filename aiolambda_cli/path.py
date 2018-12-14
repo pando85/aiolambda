aac17576d8d928f7c818acd1d634f269cc9a66e7
Pando85 <pando855@gmail.com>
2018-12-14 01:01:14 +0100

    Fix: remove .j2 in dest_files

--- aiolambda_cli/path.py
+++ aiolambda_cli/path.py
@@ -6,12 +6,12 @@ from aiolambda.functools import compose
 
 def dest_from_template(template_base: Path, dest_dir: Path, template_path: Path) -> Path:
     def remove(remove: str, base: str) -> str:
-        return base.replace(remove + '/', '')
+        return base.replace(remove, '')
 
     def add_path(base: Path, tail: Path):
         return base / tail
 
-    remove_base = partial(remove, str(template_base))
+    remove_base = partial(remove, str(template_base) + '/')
     remove_j2 = partial(remove, '.j2')
     add_dest_dir = partial(add_path, dest_dir)
 
