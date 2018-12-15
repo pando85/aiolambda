c91241f5bcb6608f653f60fd3a72fca40f03f846
pando85 <pando855@gmail.com>
2018-12-15 19:50:17 +0100

    Add template params

--- examples/postgres-publish/auth/main.py
+++ examples/postgres-publish/auth/main.py
@@ -8,7 +8,7 @@ from auth.handlers import routes
 
 
 def main():
-    app = get_app(init_db)
+    app = get_app(init_db=init_db)
     app.add_routes(routes)
 
     aiohttp.web.run_app(app, access_log=access_logger)
