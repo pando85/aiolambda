7139d5690a12631ea821953b305cf29d3201dd0a
Pando85 <pando855@gmail.com>
2018-12-15 14:12:23 +0100

    Fix type in compose args

--- aiolambda/functools.py
+++ aiolambda/functools.py
@@ -14,8 +14,7 @@ def _iscoroutinefunction_or_partial(object):
     return iscoroutinefunction(object)
 
 
-# Any becouse mypy cannot overload with List, Tupple...
-def compose(*funcs: Any) -> Callable:
+def compose(*funcs: Callable) -> Callable:
     first, second, *rest = funcs
     if rest:
         second = compose(second, *rest)
