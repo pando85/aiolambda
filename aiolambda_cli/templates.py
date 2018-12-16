ad9dd6b574bc02dc61a5c01a26ece49f25d5fe9f
pando85 <pando855@gmail.com>
2018-12-16 07:59:28 +0100

    Add types to templates.py

--- aiolambda_cli/templates.py
+++ aiolambda_cli/templates.py
@@ -1,6 +1,8 @@
+import jinja2
+
 from functools import partial
-from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
+from typing import List
 
 from aiolambda_cli.file import save_to_file
 from aiolambda_cli.path import dest_from_template
@@ -8,18 +10,18 @@ from aiolambda_cli.path import dest_from_template
 TEMPLATE_BASE_PATH = Path(__file__).parent / Path('templates')
 
 
-def render_from_file(path: Path, vars_dict):
-    env = Environment(
+def render_from_file(path: Path, vars_dict: dict) -> str:
+    env = jinja2.Environment(
         autoescape=False,
         trim_blocks=False,
-        loader=FileSystemLoader(str(path.parent)),
+        loader=jinja2.FileSystemLoader(str(path.parent)),
     )
 
     template = env.get_template(str(path.name))
     return template.render(**vars_dict)
 
 
-def render_all(vars_dict):
+def render_all(vars_dict: dict) -> List[str]:
     template_base_path = Path(TEMPLATE_BASE_PATH)
     dest_dir = Path(vars_dict['project_name'])
     dest_from_path = partial(dest_from_template, template_base_path, dest_dir)
@@ -31,14 +33,9 @@ def render_all(vars_dict):
         for directory_path in all_directories]
 
     dest_dir.mkdir()
-    [directory.mkdir() for directory in directories]
+    map(lambda x: x.mkdir(), directories)  # type: ignore
 
-    def generate_template(template_path):
-        return save_to_file(
-            render_from_file(template_path, vars_dict),
-            dest_from_path(template_path))
-
-    def filter_templates(template_path):
+    def filter_templates(template_path: Path) -> bool:
         if template_path.is_file():
             if not vars_dict['is_ci'] and 'travis' in str(template_path):
                 return False
@@ -46,7 +43,12 @@ def render_all(vars_dict):
                 return False
             return True
         return False
-
     all_templates = filter(filter_templates, all_tree)
+
+    def generate_template(template_path: Path) -> str:
+        return save_to_file(
+            render_from_file(template_path, vars_dict),
+            dest_from_path(template_path))
+
     templates = [generate_template(template_path) for template_path in all_templates]
     return templates
