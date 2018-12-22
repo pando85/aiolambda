d252e3be8efa4c3b9c4bccb3519e7949ceeef9d8
pando85 <pando855@gmail.com>
2018-12-22 18:51:17 +0100

    Fix: typo bash error in ci scripts

--- .ci/tag_version.sh
+++ .ci/tag_version.sh
@@ -11,7 +11,7 @@ if ! check_version_changes; then
 fi
 VERSION=$(python -c 'import aiolambda; print(aiolambda.__version__)')
 
-if [ ! -z "${TRAVIS_REPO_SLUG}"]; then
+if [ ! -z "${TRAVIS_REPO_SLUG}" ]; then
     git tag -a $VERSION -m "version $VERSION"
     git push "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG" $VERSION
 fi
