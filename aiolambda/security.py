55ad320ed1d507b457025b0a6e2350c83bf5f4e0
pando85 <pando855@gmail.com>
2018-12-26 23:35:08 +0100

    Fix: handle jwt error with aiohttp 403

--- aiolambda/security.py
+++ aiolambda/security.py
@@ -1,8 +1,6 @@
 import aiohttp.web
-import six
 
 from connexion.decorators.security import validate_scope as connexion_validate_scope
-from werkzeug.exceptions import Unauthorized
 from jose import JWTError, jwt
 
 from aiolambda.config import JWT_PUBLIC_KEY, JWT_ALGORITHM
@@ -11,8 +9,8 @@ from aiolambda.config import JWT_PUBLIC_KEY, JWT_ALGORITHM
 def decode_token(token, required_scopes=None):
     try:
         info = jwt.decode(token, JWT_PUBLIC_KEY, algorithms=[JWT_ALGORITHM])
-    except JWTError as e:
-        six.raise_from(Unauthorized, e)
+    except JWTError:
+        raise aiohttp.web.HTTPForbidden
     return info
 
 
