1071c8b5313a302a16e3fd62e106b242742b031d
pando85 <pando855@gmail.com>
2018-12-26 19:21:07 +0100

    Fix: create ampq client even not subscriptors defined

--- aiolambda/app.py
+++ aiolambda/app.py
@@ -3,7 +3,7 @@ import connexion
 import uvloop
 import os
 
-from typing import Callable, Dict
+from typing import Callable, Dict, Optional
 
 from aiolambda.config import API_SPECS_PATH
 from aiolambda.db import setup_db_base
@@ -13,14 +13,14 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 def get_app(init_db: Callable = None,
-            init_mq: Dict[str, Callable] = None) -> connexion.AioHttpApp:
+            init_mq: Optional[Dict[str, Callable]] = None) -> connexion.AioHttpApp:
     app = connexion.AioHttpApp(__name__)
     api = app.add_api(os.path.join(os.getcwd(), API_SPECS_PATH),
                       pass_context_arg_name='request',
                       strict_validation=True)
     if init_db:
         app.app.on_startup.append(setup_db_base(init_db, api.subapp))
-    if init_mq:
+    if isinstance(init_mq, dict):
         app.app.on_startup.append(setup_mq_base(init_mq, api.subapp))
 
     return app
