357061b6a40f5405766b32952bc05feef7dfdb1f
pando85 <pando855@gmail.com>
2018-12-26 02:45:39 +0100

    Add aiohttp validate scope

--- aiolambda/security.py
+++ aiolambda/security.py
@@ -1,6 +1,7 @@
+import aiohttp.web
 import six
 
-from connexion.decorators.security import validate_scope
+from connexion.decorators.security import validate_scope as connexion_validate_scope
 from connexion.exceptions import OAuthScopeProblem
 from werkzeug.exceptions import Unauthorized
 from jose import JWTError, jwt
@@ -13,12 +14,11 @@ def decode_token(token, required_scopes=None):
         info = jwt.decode(token, JWT_PUBLIC_KEY, algorithms=[JWT_ALGORITHM])
     except JWTError as e:
         six.raise_from(Unauthorized, e)
-
-    if required_scopes is not None and not validate_scope(required_scopes, info['scope']):
-        raise OAuthScopeProblem(
-                description='Provided user doesn\'t have the required access rights',
-                required_scopes=required_scopes,
-                token_scopes=info['scope']
-            )
-
     return info
+
+
+def validate_scope(required_scopes, token_scopes):
+    try:
+        return connexion_validate_scope(required_scopes, token_scopes)
+    except OAuthScopeProblem:
+        raise aiohttp.web.HTTPForbidden
